349db520140cf1e59947571fd7e4e7ed4083978e
Yelanika Gunasekara <yelanika.gunasekara@gmail.com>
2022-03-01 19:41:39 +0200

    Part 3 Completed: Failling Testing. Commented out

--- driver.cpp
+++ driver.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char* argv[])
 {
 	
+	//PART 1
 	int a = 10;
 	float b = 15.5;
 
@@ -20,6 +21,7 @@ int main(int argc, char* argv[])
 	std::cout << "The value of variable 'p2' is: " << p2 << std::endl;
 	std::cout << "The value of '*p2' is: " << *p2 << std::endl;
 
+	//PART 2.1
 	std::cout << std::endl << "Part 2: " << std::endl;
 
 	std::string filename;
@@ -38,6 +40,8 @@ int main(int argc, char* argv[])
 	if (!in) {
 		std::cout << "Couldn't open file " << filename << std::endl;
 	}
+
+	//PART 2.2
 
 	int *values = new int[size];
 	int n_read = 0;
@@ -63,6 +67,37 @@ int main(int argc, char* argv[])
 	std::cout << "The Average Value of " << filename << " is: " << (avg/(float)n_read) << std::endl;
 
 	delete [] values;
+	
+	//PART 3
+	// int **values2 = nullptr;
+	// values2 = new int*[size];
+	// int n_read = 0;
+
+	// for (int i = 0; i < size; ++i) {
+
+	// 	values2[i] = new int[size];
+	// 	for (int j = 0; j < size; ++j) {
+			
+	// 		in >> values2[i][j];
+	// 	}
+	// }
+
+	// in.close();
+
+	// //Calculate the average value of all the integer values in our file.
+
+	// float avg = 0.0;
+
+	// for (int i =0; i < size; ++i) {
+	// 	for (int j = 0; j < size; ++j) 
+	// 		avg += values2[i][j];
+	// }
+
+	// std::cout << "The Average Value of " << filename << " is: " << (avg/(float)n_read) << std::endl;
+
+	// for (int i = 0; i < size; ++i)
+	// 	delete [] values2[i];
+	// delete [] values2;
 
 	return 0;
 }
